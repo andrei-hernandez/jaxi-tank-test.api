819a7ceeb214b82e741de5e30b0073b2d748a4fa
AndiUchiha1 <andiuchihahaha@gmail.com>
2021-06-30 02:59:02 +0000

    solved error in LogIn

--- src/resolvers/queries/accountLogIn.ts
+++ src/resolvers/queries/accountLogIn.ts
@@ -13,12 +13,14 @@ export const logIn = async (userInput: UserLogin): Promise<sessionData> => {
 
   const isEqual = await compare(userInput?.password, User?.password); //compare is a method from bcript to compare the encrypted password
   if (isEqual === undefined || isEqual === null || isEqual === false) {
-    return { userId: '-1', token: '', tokenExpiration: -1, err: { errorCode: 2, errorDesc: "Incorrect password" } }
+    return { userId: '-1', token: '', avatar: '', userName: '', tokenExpiration: -1, err: { errorCode: 2, errorDesc: "Incorrect password" } }
   }
 
   const token = sign({ userId: User?._id, email: User?.email }, 'somesupersecretkey', {//here create the jsw too the session and return the session data to the client
     expiresIn: '1d'//ignore the "somesupersecretkey"
   });
+
+
 
   return { userId: User?._id, avatar: User?.avatar ? User?.avatar : avatarDefaultUri, userName: User.userName, token: token, tokenExpiration: 1 }
 }
